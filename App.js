8c48a1345ac92bdb0275f628fba584cc8c93ca6c
kthatoto <kthatoto@gmail.com>
2019-06-01 01:48:14 +0900

    Use store

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Provider } from 'react-redux'
 import AppContainer from './src/AppContainer'
+import store from './src/redux/store'
 
 export default class App extends React.Component {
   render () {
-    return <AppContainer />
+    return (
+      <Provider store={store()}>
+        <AppContainer />
+      </Provider>
+    )
   }
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,7 @@ import { createStore, applyMiddleware } from 'redux'
 
 import reducer from './reducer'
 
-export default createStoreWithMiddleware = () => {
+export default () => {
   const store = applyMiddleware(
     // thunkMiddleware
   )(createStore)
