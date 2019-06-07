aa7e3d3c1c4065b9585feea6ea70418a44efd113
kthatoto <kthatoto@gmail.com>
2019-06-07 09:34:14 +0900

    Add profile edit screen

--- src/views/settings/ProfileScreen.js
+++ src/views/settings/ProfileScreen.js
@@ -1,14 +1,52 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, TouchableOpacity, StyleSheet, FlatList } from 'react-native'
+import { Formik } from 'formik'
+
+import ApiClient from '../../utils/ApiClient'
+
+const profileItems = {
+}
 
 class ProfileScreen extends React.Component {
+  updateProfile () {
+  }
+
   render () {
     return (
       <View style={{flex: 1}}>
-        <Text>OK</Text>
+        <FlatList
+          data={profileItems}
+          renderItem={(item, index) => {
+            <View></View>
+          }}
+        />
       </View>
     )
   }
+
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: 'プロフィール設定',
+      headerRight: (
+        <TouchableOpacity onPress={this.updateProfile}>
+          <Text style={styles.headerText}>更新</Text>
+        </TouchableOpacity>
+      ),
+      headerLeft: (
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Text style={styles.headerText}>キャンセル</Text>
+        </TouchableOpacity>
+      )
+    }
+  }
 }
+
+const styles = StyleSheet.create({
+  headerText: {
+    color: 'white',
+    marginHorizontal: 10,
+    fontWeight: 'bold'
+  }
+})
 
 export default ProfileScreen
